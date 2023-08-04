6f77e373035ad5f4523a8bcda86abfeca6002e33
ArtSatanov <artsatanov@gmail.com>
2023-08-04 19:35:25 +0300

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+function gallryMarkup (arr) { 
+   return arr.map(({ preview, original, description }) => 
+   `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+   ).join('')
+}; 
+
+console.log(gallryMarkup(galleryItems));
